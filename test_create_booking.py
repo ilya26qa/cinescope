db8b5834570b765cfd0cfefcde4969313b8abb83
i.listratenkov <i.listratenkov@napopravku.ru>
2025-06-25 14:56:15 +0800

    fix

--- test_create_booking.py
+++ test_create_booking.py
@@ -29,9 +29,9 @@ class TestBookings:
         get_booking = auth_session.get(f"{BASE_URL}/booking/{booking_id}")
         assert get_booking.status_code == 404, "Бронь не удалилась"
 
-    def test_change_all_data_booking1(self, auth_session, booking_data):
+    def test_change_all_data_booking(self, auth_session, booking_data):
+        # может создание бронирование нужно вынести в фикстуру?
         create_booking = auth_session.post(f"{BASE_URL}/booking", json=booking_data)
-        assert create_booking.status_code == 200, "Ошибка при создании брони"
 
         booking_id = create_booking.json().get("bookingid")
         assert booking_id is not None, "Идентификатор брони не найден в ответе"
@@ -61,8 +61,32 @@ class TestBookings:
                 new_payload["bookingdates"]["checkin"]), 'дата заезда не совпадает'
         assert (change_booking.json()["bookingdates"]["checkout"] ==
                 new_payload["bookingdates"]["checkout"]), 'дата выезда не совпадает'
+        assert change_booking.json()['additionalneeds'] == new_payload["additionalneeds"], 'доп услуга не совпадает'
         # третий вариант
         for key, value in new_payload.items():
             assert change_booking.json().get(key) == value, f'{key} не совпадает'
 
+    def test_patch_booking(self, auth_session, booking_data):
+        create_booking = auth_session.post(f"{BASE_URL}/booking", json=booking_data)
+        booking_id = create_booking.json().pop("bookingid")
+
+        new_payload = {
+            "firstname": faker.first_name(),
+            "lastname": faker.last_name(),
+            "bookingdates": {
+                "checkin": "2021-04-05",
+                "checkout": "2022-04-08"
+            }
+        }
+        patch_booking = auth_session.patch(f"{BASE_URL}/booking/{booking_id}", json=new_payload)
+        assert patch_booking.status_code == 200, 'не удалось пропатчить'
+
+        get_booking = auth_session.get(f"{BASE_URL}/booking/{booking_id}")
+
+        for key, value in get_booking.json().items():
+            if key in new_payload:
+                assert new_payload[key] == get_booking.json().get(key), f'значение {key} не изменилось, а должно'
+            else:
+                assert value == create_booking.json()["booking"].get(key), (f'изменилось значение {key}, которое не'
+                                                                            f' должно меняться ')
 
